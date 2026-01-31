cf240173c1d0948afaa84ab70972e3359346c09f
Jose Tamayo <jose.tamayo@canpar.io>
2026-01-31 09:39:12 -0300

    otro commit

--- api.bootcamp.clt/Aplication/Dto/ProductoController.cs
+++ api.bootcamp.clt/Aplication/Dto/ProductoController.cs
@@ -1,1 +1,7 @@
+/// <summary>
+/// Representa un producto dentro del sistema.
+/// </summary>
+/// <param name="Id">Identificador único del producto.</param>
+/// <param name="Name">Nombre del producto.</param>
+/// <param name="Price">Precio del producto.</param>
 public record ProductDTO(int Id, string Name, decimal Price);
